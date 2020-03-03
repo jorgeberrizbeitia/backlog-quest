bb179ae75c2b28e8511008db422901ab9bb2fa79
Jorge Berrizbeitia <jorgeberrizbeitia@gmail.com>
2020-03-03 16:58:34 +0100

    added auth

---
+++ src/components/AnonRoute.js
@@ -0,0 +1,27 @@
+//	components/AnonRoute.js
+
+// gives you public pages/components
+
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { withAuth } from "../lib/Auth";
+
+function AnonRoute({ component: Component, isLoggedIn, ...rest }) {
+  return (
+    // <Route
+    //   {...rest}
+    //   render={props =>
+    //     !isLoggedIn ? <Component {...props} /> : <Redirect to="/private" />
+    //   }
+    // />
+    <Route
+      {...rest}
+      render={function(props) {
+        if (isLoggedIn) return <Redirect to="/private" />;
+        else if (!isLoggedIn) return <Component {...props} />;
+      }}
+    />
+  );
+}
+
+export default withAuth(AnonRoute);
